0c7b345b141391c8e588b15a264fe8fba0bcae55
DilkhushGupta <guptadilkhush3@gmail.com>
2024-08-04 21:11:35 +0530

    Update 1.0

---
+++ jsx_challenge1/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <>
+    <h1>Top 5 My Indian Dishes.</h1>,<p>Here is my top Indian Dish</p>,
+    <ol>
+      <li>Dal-Bhat</li>
+      <li>Litti Chokha</li>
+      <li>Idli Sambhar</li>
+      <li>Dhosa Sambhar</li>
+      <li>Samosa</li>
+    </ol>
+  </>,
+  document.getElementById("root")
+);
